30859d982015b4c0c536c1883110353b0cd1d476
Michael Bates <michaelbates_90@yahoo.com>
2018-11-27 01:14:28 +0000

    added the user_stocks association for many to many tables and also fixed a small double flash error in the results search

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,4 +1,6 @@
 class Stock < ActiveRecord::Base
+  has_many :user_stocks
+  has_many :users, through: :user_stocks
   
   def self.new_from_lookup(ticker_symbol)
     begin
@@ -9,6 +11,8 @@ class Stock < ActiveRecord::Base
     end
   end
   
-  
+  def self.find_by_ticker(ticker_symbol)
+    where(ticker: ticker_symbol).first
+  end
   
 end
