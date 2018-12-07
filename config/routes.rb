aca648469dbca922090f9e713903fcdf5e7685c0
Michael Bates <michaelbates_90@yahoo.com>
2018-12-07 01:53:23 +0000

    added a route path for the user/registration and made the registration user controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   
   
-  devise_for :users
+  devise_for :users, :controllers => { :registrations => "user/registrations" }
   root 'users#my_portfolio'
   get 'my_portfolio', to: "users#my_portfolio"
   get 'search_stocks', to: 'stocks#search'
